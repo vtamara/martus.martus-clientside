1481e73772942e574e4e85cc595b50f7936dbef9
charles <charles@localhost>
2005-08-19 22:24:45 +0000

    non translated strings in mtf

--- source/org/martus/clientside/UiLocalization.java
+++ source/org/martus/clientside/UiLocalization.java
@@ -135,15 +135,18 @@ public class UiLocalization extends MtfAwareLocalization
 		writer.writeln("# 24.  do not translate \"#MtfLanguage#\" (used for Incompatible Mtf Version to Program Version Comparison)");
 		writer.writeln("# 25.  do not translate \"#MtfVersionNumber#\" (used for Incompatible Mtf Version to Program Version Comparison)");
 		writer.writeln("# 26.  do not translate \"#ProgramVersionNumber#\" (used for Incompatible Mtf Version to Program Version Comparison)");
+		writer.writeln("# 27.  do not translate \"#MoveFolderUp#\" (used for Folder organization)");
+		writer.writeln("# 28.  do not translate \"#MoveFolderUp#\" (used for Folder organization)");
+		
 		if(rightToLeftLanguages.contains(languageCode))
 		{
-			writer.writeln("# 27.  If you want your language to appear left to right,");
+			writer.writeln("# 29.  If you want your language to appear left to right,");
 			writer.writeln("#      Comment the next line (by adding a # in front of !right-to-left)");
 			writer.writeln("!right-to-left");
 		}
 		else
 		{
-			writer.writeln("# 27.  If you want your language to appear right to left,");
+			writer.writeln("# 29.  If you want your language to appear right to left,");
 			writer.writeln("#      Uncomment the next line (by removing the # in front of #!right-to-left)");
 			writer.writeln("#!right-to-left");
 		}
