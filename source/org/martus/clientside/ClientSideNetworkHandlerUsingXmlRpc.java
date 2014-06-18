e64784625fde66b127bf28b915503aa9597e6216
Kevin Smith <Kevin Smith@localhost>
2014-06-18 13:51:11 -0700

    Refactor rename method

--- source/org/martus/clientside/ClientSideNetworkHandlerUsingXmlRpc.java
+++ source/org/martus/clientside/ClientSideNetworkHandlerUsingXmlRpc.java
@@ -303,7 +303,7 @@ public class ClientSideNetworkHandlerUsingXmlRpc
 		try 
 		{
 			InetAddress address = InetAddress.getByName(serverName);
-			if(address != null && address.isSiteLocalAddress() && getTransport() != null && getTransport().isEnabled())
+			if(address != null && address.isSiteLocalAddress() && getTransport() != null && getTransport().isTorEnabled())
 			{
 				MartusLogger.log("Orchid cannot reach local address: " + serverName);
 				return null;
